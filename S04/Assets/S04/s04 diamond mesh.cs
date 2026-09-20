226390b25be2bdf1a825d121085aeaf33a198447
ayj201608 <ayj201608@ewha.ac.kr>
2026-09-20 21:22:52 +0900

    s04 commit 2

---
+++ S04/Assets/S04/s04 diamond mesh.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class S04_CustomDiamondMesh : MonoBehaviour
+{
+    void Start()
+    {
+        Vector3[] vertices = new Vector3[]
+        {
+            
+        };
+
+        int[] triangles = new int[]
+        {
+        };
+
+        Mesh mesh = new Mesh();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
+
+        GetComponent<MeshFilter>().mesh = mesh;
+        GetComponent<MeshRenderer>().sharedMaterial = new Material(Shader.Find("Universal Render Pipeline/Lit"));
+    }
+}
