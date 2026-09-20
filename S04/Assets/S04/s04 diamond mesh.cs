d465ddbe4dcf2583287215284cfc09fac8d21549
ayj201608 <ayj201608@ewha.ac.kr>
2026-09-20 21:32:12 +0900

    s04 commit 3

--- S04/Assets/S04/s04 diamond mesh.cs
+++ S04/Assets/S04/s04 diamond mesh.cs
@@ -5,13 +5,31 @@ public class S04_CustomDiamondMesh : MonoBehaviour
 {
     void Start()
     {
+        // 정점 6개 - 위/아래 꼭짓점 2개 + 가운데 4개
+    
         Vector3[] vertices = new Vector3[]
         {
-            
+            new Vector3(0.5f, 0f, 0.5f), // 0: 아래
+            new Vector3(0.5f, 1f, 0.5f), // 1: 위
+            new Vector3(0f,   0.5f, 0f), // 2: 앞왼
+            new Vector3(1f,   0.5f, 0f), // 3: 앞오른
+            new Vector3(1f,   0.5f, 1f), // 4: 뒤오른
+            new Vector3(0f,   0.5f, 1f), // 5: 뒤왼
         };
 
         int[] triangles = new int[]
         {
+            // 위
+            1, 2, 3,
+            1, 3, 4,
+            1, 4, 5,
+            1, 5, 2,
+
+            // 아래
+            0, 3, 2,
+            0, 4, 3,
+            0, 5, 4,
+            0, 2, 5,
         };
 
         Mesh mesh = new Mesh();
