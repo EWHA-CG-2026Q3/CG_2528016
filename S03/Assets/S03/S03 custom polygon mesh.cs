59034c31a27dcd5bea1a9754b0d5cabc905c0a7b
ayj201608 <ayj201608@ewha.ac.kr>
2026-09-20 20:31:24 +0900

    s03 commit 3

--- S03/Assets/S03/S03 custom polygon mesh.cs
+++ S03/Assets/S03/S03 custom polygon mesh.cs
@@ -7,12 +7,18 @@ public class S03_CustomPolygonMesh : MonoBehaviour
     {
         Vector3[] vertices = new Vector3[]
         {
-            
+            new Vector3(0f, 1f, 0f),
+            new Vector3(0.95f, 0.31f, 0f),
+            new Vector3(0.59f, -0.81f, 0f),
+            new Vector3(-0.59f, -0.81f, 0f),
+            new Vector3(-0.95f, 0.31f, 0f)
         };
 
         int[] triangles = new int[]
         {
-            
+            0, 1, 2,
+            0, 2, 3,
+            0, 3, 4
         };
 
         Mesh mesh = new Mesh();
